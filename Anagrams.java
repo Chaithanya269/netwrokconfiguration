200f8692bbc049526c4fd650167c2a0978185f8a
Chaithanya269 <chaithanyareddymv@gmail.com>
2025-02-13 21:21:13 +0530

    Create Anagrams.java

---
+++ Anagrams.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String sortedKey = new String(charArray);
+            map.putIfAbsent(sortedKey, new ArrayList<>());
+            map.get(sortedKey).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+}
